1c4077b3c5d34e1cc6a54432e451a678940734ba
mikldrouet-max <mikl.drouet@gmail.com>
2026-06-20 02:48:37 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-/* Cap. — Service Worker v6 */
+/* Cap. — Service Worker v7 */
 const CACHE = "cap-v2";
 const SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
