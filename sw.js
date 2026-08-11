40643aa6dd8b113de2403ceae11ea0d7d49a7cdc
mikldrouet-max <mikl.drouet@gmail.com>
2026-08-11 12:20:50 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Cap. — Service Worker v22 — réseau d'abord pour la page */
-const CACHE = "cap-v21";
+const CACHE = "cap-v22";
 const SHELL = ["./", "./index.html", "./manifest.json", "./favicon.svg", "./icon-192.png", "./icon-512.png"];
 
 // Installation : mettre le shell en cache + activer tout de suite
